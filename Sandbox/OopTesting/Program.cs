6748e366eda4a63b41282a70de154e1cb4b268a7
Debugieman <wsarzynski@gmail.com>
2022-07-13 00:51:36 +0200

    experimenting
    exping

--- Sandbox/OopTesting/Program.cs
+++ Sandbox/OopTesting/Program.cs
@@ -1,10 +1,29 @@
 ﻿var names = new List<string> { "name", "Anna", "Basia", "Filip" };
+
+// dodawanie do listy 
 names.Add("Maria");
+names.Add("Marek");
+names.Add("Patryk");
+names.Add("Anielka");
+names.Add("Ania");
+// foreach string
+foreach (string name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}");
+}
+
+// wyciąganie z listy
+var index = names.IndexOf("Basia");
+Console.WriteLine(index);
 
 
+// Wyszukiwanie w liscie  (daje [-1] kiedy nie ma w liście czego szukamy )
+Console.WriteLine(names[0]);
 
-
-
-
-
-Console.WriteLine(names);    
+// Sorotwanie string metodą name.Sort(); na zasadzie A<B<C<D
+/*names.Sort();
+foreach ( name in names)
+{
+    Console.WriteLine($"Hello {names.ToUpper)  !");
+ }
+/*
---
+++ Sandbox/OtherExcercises/Program.cs
@@ -0,0 +1,7 @@
+﻿var FibonacciNumbers = new List<int> { 1, 1 };
+var previous1 = FibonacciNumbers[FibonacciNumbers.Count - 1];
+var previous2 = FibonacciNumbers[FibonacciNumbers.Count - 2];
+FibonacciNumbers.Add(previous1 + previous2);
+
+foreach (var item in FibonacciNumbers)
+    Console.WriteLine(item);
