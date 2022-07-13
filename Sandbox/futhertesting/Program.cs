dc36b1305c5020f7e40fbc704fd402e4b1bdffe7
Debugieman <wsarzynski@gmail.com>
2022-07-13 20:58:05 +0200

    Fibonacci with input ready
    ready project that returns the number of FIbonacci sequence that u ask for

---
+++ Sandbox/futhertesting/Program.cs
@@ -0,0 +1,18 @@
+﻿var fibonacciNumbers = new List<int> { 1, 1 };
+Console.WriteLine("Wich Fibonacci number would u like to know? ");
+var x = Console.ReadLine();
+int num_x = Int32.Parse(x);
+
+
+
+while (fibonacciNumbers.Count <num_x )
+{
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+    fibonacciNumbers.Add(previous + previous2);
+}
+
+
+
+foreach (var item in fibonacciNumbers)
+    Console.WriteLine(item);
