4e29fea4718cdd7edc7a9b0fc339f264972f8eda
Debugieman <wsarzynski@gmail.com>
2022-07-14 21:05:12 +0200

    cdn
    cdn

--- Sandbox/ObjectOrientedProgramming/Bankaccount.cs
+++ Sandbox/ObjectOrientedProgramming/Bankaccount.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace ObjectOrientedProgramming
 {
-    internal class Bankaccount
-    {
-        public stringNumber {}
+    public class Bankaccount
+    {// below properties
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        public Bankaccount(string name, decimal initialBalance, string number)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+
+        }
+
+        //below methods
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            //xx
+        }
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        { }
+
     }
+
+
+
+
 }
+
