45bb91245ac9a9e5f292f5a85aa94a67da6daac1
Debugieman <wsarzynski@gmail.com>
2022-07-17 15:30:56 +0200

    Update Program.cs

--- OPPCD/OPPCD/Program.cs
+++ OPPCD/OPPCD/Program.cs
@@ -1,11 +1,30 @@
 ﻿Console.WriteLine("Enter a string and press enter");
-string readInput = Console.ReadLine();  
-Console.WriteLine($"You have entered {0}",readInput);
+string readInput = Console.ReadLine();
+Console.WriteLine($"You have entered {0}", readInput);
 
 Console.WriteLine("Enter a string and press enter");
 int asciiValue = Console.Read();
-Console.WriteLine($"asciiValue value is {0}",asciiValue);
+Console.WriteLine($"asciiValue value is {0}", asciiValue);
 
 
+
+/*
+
+public static void Main(string[] args)
+{
+    float new_stringForFloat;
+    int new_stringForInt;
+    string stringForFloat = "0.85"; // datatype should be float
+    string stringForInt = "12345"; // datatype should be int
+    new_stringForFloat = float.Parse(stringForFloat);
+    new_stringForInt = int.Parse(stringForInt); 
+
+
+
+
+
+
+
+}
 
 Console.ReadKey);
