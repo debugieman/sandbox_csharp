4f14a4a17337b6476ef2f3bf0a5e41cec2a642d1
Debugieman <wsarzynski@gmail.com>
2022-07-18 13:28:46 +0200

    stringmanipulation
    Learning basic string manipulation techiniques in c#10

---
+++ StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("PLease enter your name and press enter: ");
+string myName = Console.ReadLine();
+string myNameUpper = String.Format($"Upper case: {myName.ToUpper()}");
+Console.WriteLine(myNameUpper);
+string myNameLower = String.Format($"Lower case: {myName.ToLower()}");
+Console.WriteLine(myNameLower);
+Console.ReadKey();
+
+
+Console.WriteLine("Enter a stringhere: ");
+string input = Console.ReadLine();
+Console.WriteLine("Enter a character you wanna search for:  ");
+char searchInput = Console.ReadLine()[0];
+int searchIndex = input.IndexOf(searchInput);
+Console.WriteLine($"Index of searched character is {searchInput}, counting from 0 ");
+
