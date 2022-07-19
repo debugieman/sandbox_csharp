4fa01a5370cbfafa2a34b05fdc6bd442082b4a13
Debugieman <wsarzynski@gmail.com>
2022-07-19 22:02:37 +0200

    cd
    cd

--- StringManipulation/StringManipulation/Program.cs
+++ StringManipulation/StringManipulation/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("PLease enter your name and press enter: ");
+﻿/*Console.WriteLine("PLease enter your name and press enter: ");
 string myName = Console.ReadLine();
 string myNameUpper = String.Format($"Upper case: {myName.ToUpper()}");
 Console.WriteLine(myNameUpper);
@@ -13,4 +13,24 @@ Console.WriteLine("Enter a character you wanna search for:  ");
 char searchInput = Console.ReadLine()[0];
 int searchIndex = input.IndexOf(searchInput);
 Console.WriteLine($"Index of searched character is {searchInput}, counting from 0 ");
+
+
+*/
+
+const double PI = 3.14159265359;
+string greet = "lol";
+void PrintString()
+{
+
+    Console.WriteLine(greet.ToLower() + greet.ToUpper());
+
+
+};
+
+
+
+PrintString();
+
+
+
 
