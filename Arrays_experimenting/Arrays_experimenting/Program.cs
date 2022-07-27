9b6b88d5d53ed39da44f8589ee15dc03772747cf
Debugieman <wsarzynski@gmail.com>
2022-07-27 19:48:29 +0200

    making decisions
    making decition practising

--- Arrays_experimenting/Arrays_experimenting/Program.cs
+++ Arrays_experimenting/Arrays_experimenting/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[,] Cars = { 
+                   {"Bugatti", "Mercedes", "Porche", "Ferrari", "Cadilac" }, 
+                   {"Wolksvagen", "Nyska", "Mazda", "GrandCherokee", "Lincon" }, 
+};
+
+Console.WriteLine(Cars[0,1]);
+
+
+foreach (string car in Cars)
+{
+    Console.WriteLine($"The brand of {car} cars is raising in value");
+};
+
+for (int i = 0; i < Cars.GetLength(0); i++)
+{
+    for (int j = 0; j < Cars.GetLength(1); j++)
+    {
+        
+        Console.WriteLine(Cars[i, j]);
+    }
+};
+
+Console.ReadKey();
---
+++ switch_statement/switch_statement/Program.cs
@@ -0,0 +1,46 @@
+﻿
+Console.WriteLine("How old are you ?");
+string age = Console.ReadLine();
+
+var agex =  int.Parse(age);
+switch (agex)
+{
+    case 15:
+        Console.WriteLine("To young");
+        break;
+    case 25:
+        Console.WriteLine("good to go ");
+        break;
+    default:
+        Console.WriteLine("how old are u?");
+        break;
+}
+Console.ReadKey();
+
+
+int temperature = 101;
+string stateOfMatter;
+
+
+
+if (temperature < 0)
+    stateOfMatter = "solid";
+else
+    stateOfMatter = "liquid";
+Console.WriteLine($"State of matter is {stateOfMatter}");
+
+
+/*
+stateOfMatter = temperature < 0 ? "solid" : "liquid";
+Console.WriteLine($"state of matter is {stateOfMatter}");
+*/
+
+if (temperature < 0)
+    stateOfMatter = "solid";
+else if (temperature >= 0 && temperature < 100) 
+    stateOfMatter = "liquid";
+else 
+    stateOfMatter = "gas";
+Console.WriteLine(stateOfMatter);
+
+Console.ReadKey();
