b4028aba6276e0cd2b39ed320f7f78f5fab04f25
Debugieman <wsarzynski@gmail.com>
2022-08-06 00:15:14 +0200

    woring on oop project

--- car_try_console/car_try_console/Car.cs
+++ car_try_console/car_try_console/Car.cs
@@ -13,36 +13,54 @@ namespace car_try_console
     {
         //const and fields
         
-        private static int _numberOfCars = 0;
-        private string _name { get; set; }
-        private string _modelname { get; set; }
-        private int _numberOfDoors { get; }
-        private int _engineCapacity { get; }
-        private double _averageFuelConsumption { get; }
+        public static int _numberOfCars;
+        private string _name;
+        private string _modelname;
+        public int _numberOfDoors;
+        private int _engineCapacity;
+        private double _averageFuelConsumption;
         
-
         //properties
-
-        
-        public string Name {get { return _name; }}
+        public string Name { get { return _name; } }
         public string Modelname { get { return _modelname; } }
 
-        public string NumberOfDoors { get { return _numberOfDoors; } }
-        public int EngineCapacity { get { return _engineCapacity; } }
+        public int NumberOfDoors { get { return _numberOfDoors; } }
+        public int EngineCapacity  { get { return _engineCapacity; } }
         public double AverageFuelConsumption { get { return _averageFuelConsumption; } }
-
+        // ponizej czy tak tego uzywac ?
         /// <summary>
         /// Constructor
         /// </summary>
+        /// 
         /// <param name="name">This is name of the car's brand.Name of manufacturer(for example Toyota)</param>
         /// <param name="modelname">This is model of certain brand's car (for example Avensis) </param>
         /// <param name="numberOfDoors">It's number of doors with windowpane, that you can access a car with(it's usually number between 2 and 5)</param>
         /// <param name="enginecapacity">is calculated by multiplying together three values; the distance travelled by the piston (the stroke length), the circular area of the cylinder, and the number of cylinders in the whole engine(usually between 1.0 and 6.0)</param>
         /// <param name="averageFuelConsumption">Fuel consumption measures the amount of fuel a car consumes to go a specific distance(In this example, its liters per 100km)</param>
-        public Car(string name, string modelname, int numberOfDoors, int enginecapacity, double averageFuelConsumption)
-        {
 
-        }
+
+
+
+
+
+        public Car() { }
+        
+
+        public Car(string name,
+                   string modelname,
+                   int numberOfDoors,
+                   int enginecapacity,
+                   double averageFuelConsumption)
+        {
+            _numberOfCars++;
+            _name = name;
+            _modelname = modelname;
+            _numberOfDoors = numberOfDoors;
+            _engineCapacity = enginecapacity;
+            _averageFuelConsumption = averageFuelConsumption;
+
+
+    }
         
      
       
--- car_try_console/car_try_console/Program.cs
+++ car_try_console/car_try_console/Program.cs
@@ -1,9 +1,27 @@
 ﻿
 using car_try_console;
 
-var car = new Car();
+/*
+static string[] ReadFile(string filename)
+{
+    string[] files = System.IO.File.ReadAllLines(filename);
+    return lines;
+}
 
-car.FuelPrice(5, 50);
-car.
+*/
 
 
+var autoWiktora = new Car("Toyota", "Avensis", 4, 3, 2.2);
+var autoBartka = new Car("Bartekmobile", "SuperCustomMade", 2, 3, 10.1);
+var autoPatryka = new Car("Porsche", "Carrera", 5, 5, 15.0);
+
+
+Console.WriteLine($"Our Magnificent factory created new {autoWiktora.Name}, {autoWiktora.Modelname} ");
+Console.WriteLine($"Our Magnificent factory created new {autoBartka.Name}, {autoBartka.Modelname} ");
+Console.WriteLine($"Our Magnificent factory created new {autoPatryka.Name}, {autoPatryka.Modelname} ");
+
+
+Console.WriteLine(Car._numberOfCars);
+
+
+Console.ReadKey();
