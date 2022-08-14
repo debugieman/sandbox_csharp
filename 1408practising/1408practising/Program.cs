1db9f6e32fcbb531ec37684fc4ef1bdbcb67dc29
Debugieman <wsarzynski@gmail.com>
2022-08-14 11:30:37 +0200

    practising methods and arrays

---
+++ 1408practising/1408practising/Program.cs
@@ -0,0 +1,35 @@
+﻿//declare jagged array (3 arrays inside this array)
+int[][] jaggedarray = new int[3][];
+//def rows
+jaggedarray[0] = new int[5];
+jaggedarray[1] = new int[3];
+jaggedarray[2] = new int[2];
+//adding value 
+jaggedarray[0] = new int[]  { 2, 3, 4, 5, 6 };
+jaggedarray[0] = new int[] { 5 };
+jaggedarray[0] = new int[] { 6, 11 };
+
+
+// 2nd option of adding
+int[][] jaggedArray2 = new int[][]
+
+{
+new int[] {2,4,6,7,8 },
+new int[] {2,4,8}
+
+
+};
+
+Console.WriteLine($"The value is {jaggedArray2[0][2]}");
+
+for (int i = 0; i < jaggedArray2.Length; i++)
+{
+    Console.WriteLine($"Element {i}");
+    for ( int j = 0; j < jaggedArray2[i].Length; j++ )
+    Console.WriteLine(jaggedArray2[i][j]);
+
+}
+
+
+
+Console.ReadKey();
---
+++ Members/Members/Class1.cs
@@ -0,0 +1,57 @@
+﻿namespace Members
+{
+    public class Class1
+    {
+        //member private field:
+        private string memberName;
+        public string jobTitle;
+        private int salary = 20000;
+
+        //member public field
+        public int age;
+        // member - property - exposes jobTitle safely - properties start with Capital Letter 
+        public string JobTitle
+        {
+            get 
+            {
+                return jobTitle; 
+            }
+            set
+            {
+                jobTitle = value;
+            }
+        public void Introducing (bool isfriend)
+
+        {
+            if (isfriend = True)
+            {
+                SharingPrivateInfo();
+
+            }
+            else
+            {
+                Console.WriteLine($"Hi my name is {memberName} , and im years old");
+            }
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine($"My salary is {salary}");
+        }
+
+
+        public  void Members()
+        {
+            age = 30;
+            memberName = "Lucy";
+            salary = 60000;
+            jobTitle = "Cook";
+
+
+            Console.WriteLine("Object created");
+       
+        }
+        // member finalizer destructor
+
+         
+
