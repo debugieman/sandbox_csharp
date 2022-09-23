9e06c90a7d363986c8992caea897886616436769
Debugieman <wsarzynski@gmail.com>
2022-09-23 11:09:27 +0200

    1

---
+++ PostProject/PostProject/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostProjectID;
+
+public  class Post
+{
+    private static int currentPostId;
+
+    //properties
+    protected int ID { get; set; }
+    protected string Title { get; set; }  
+    protected string SendByUserName { get; set; }   
+    protected bool IsPublic { get; set; }
+    public Post()
+    { 
+        ID = 0;
+        Title = "My first obj";
+        SendByUserName = "Me";
+        IsPublic = true;
+    }
+
+
+
+
+}
