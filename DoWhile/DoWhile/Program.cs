67b2e59401547b91805d0e95cd0b41f72f43f1e2
Debuggieman <107075050+debugieman@users.noreply.github.com>
2022-10-13 13:51:32 +0200

    changing via VS

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { string[] cars = { "Toyota", "Lambo", "Audi" };
+
+
+
+
+            int i = 0;
+            while (i < cars.Length)
+            {
+                Console.WriteLine(cars[i]);
+                i++;
+                if (cars[i] == "Lambo")
+                {
+                    Console.WriteLine("break");
+                    break;
+                }
+            }
+            Console.WriteLine("**************************");
+
+            for (int j = 0; j < cars.Length; j++)
+            {
+
+
+
+                Console.WriteLine(cars[i])
+    
+
+
+
+
+
+
+        }
+       
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
---
+++ Switch _S/Switch _S/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Xml;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        switch (DateTime.Now.DayOfWeek)
+        {
+            case DayOfWeek.Monday:
+                Console.WriteLine("It's Monday");
+                break;
+            case DayOfWeek.Friday:
+                Console.WriteLine("Last day of the week");
+                break;
+
+
+            default:
+                Console.WriteLine("other day");
+                break;
+        }
+
+
+        Console.WriteLine("*************************");
+
+        string userInput = Console.ReadLine();
+        int yearOfBirth;
+        if (int.TryParse(userInput, out yearOfBirth))
+        {
+            int age = DateTime.Now.Year - yearOfBirth;
+            Console.WriteLine("You are" + age);
+
+        }
+        else
+
+        {
+            Console.WriteLine("Please enter again");
+
+
+        }
+
+        Console.WriteLine("******************************");
+        double doubleV =  -3.5;
+        int intV = (int)doubleV;
+        Console.WriteLine(doubleV);
+
+
+
+
+
+
+        Console.WriteLine(" ************  TABLICE / Arrays ********** ");
+
+        string[] cars = { "BMW", "Merc", "Audi" };
+
+        int i = 0;
+        while (i <= 2)
+        {
+            Console.WriteLine(i);
+            i++; 
+        }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }   
+
+    
+
+
+}
