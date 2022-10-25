881aa083c343528c7f90c00281ae175ee9233275
Debuggieman <107075050+debugieman@users.noreply.github.com>
2022-10-25 20:44:21 +0200

    fixing bugs in  project.

--- car_try_console/car_try_console/Car.cs
+++ car_try_console/car_try_console/Car.cs
@@ -12,7 +12,7 @@ namespace car_try_console
     public class Car
     {
         //const and fields
-        
+
         public static int _numberOfCars;
         private string _name;
         private string _modelname;
@@ -27,46 +27,30 @@ namespace car_try_console
         public string Modelname { get { return _modelname; } }
 
         public int NumberOfDoors { get { return _numberOfDoors; } }
-        public int EngineCapacity  { get { return _engineCapacity; } }
-        public double AverageFuelConsumption 
-        {
-            get  { return _averageFuelConsumption; } 
-            set { _averageFuelConsumption = value; }
-        
-        
-        }
+        public int EngineCapacity { get { return _engineCapacity; } }
+        public double AverageFuelConsumption { get; set; }
 
-        public double FuelPrice { get;  set; }
+
+        public double FuelPrice { get; set; }
 
         public double Cost
         {
             get { return _averageFuelConsumption * fuelPrice; }
-            
+
 
 
         }
 
-    }    
 
-         public Car() { }
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// 
-        /// <param name="name">This is name of the car's brand.Name of manufacturer(for example Toyota)</param>
-        /// <param name="modelname">This is model of certain brand's car (for example Avensis) </param>
-        /// <param name="numberOfDoors">It's number of doors with windowpane, that you can access a car with(it's usually number between 2 and 5)</param>
-        /// <param name="enginecapacity">is calculated by multiplying together three values; the distance travelled by the piston (the stroke length), the circular area of the cylinder, and the number of cylinders in the whole engine(usually between 1.0 and 6.0)</param>
-        /// <param name="averageFuelConsumption">Fuel consumption measures the amount of fuel a car consumes to go a specific distance(In this example, its liters per 100km)</param>
 
-     /*       public Car(string name,
-                   string modelname,
-                   int numberOfDoors,
-                   int enginecapacity,
-                   double averageFuelConsumption)
+        public Car(string name,
+                          string modelname,
+                          int numberOfDoors,
+                          int enginecapacity,
+                          double averageFuelConsumption)
         {
-        
+
             _numberOfCars++;
             _name = name;
             _modelname = modelname;
@@ -74,25 +58,21 @@ namespace car_try_console
             _engineCapacity = enginecapacity;
             _averageFuelConsumption = averageFuelConsumption;
 
-  */
-    
 
-        
-        public double CalculateFuel (double lenghtRoad)
+             double CalculateCost(double lenght, double fuelPrice)
+            {
+                return (AverageFuelConsumption * lenghtRoad / 100) * FuelPrice;
+
+
+            }
+
+        }
+
+        private double CalculateFuel(double lenghtRoad)
         {
             return AverageFuelConsumption * lenghtRoad / 100;
 
         }
-
-
-        public  double CalculateCost(double lenght, double fuelPrice)
-        {
-            return (AverageFuelConsumption * lenghtRoad / 100) * FuelPrice;
-
-
-        }
-
-          
 
 
 
@@ -102,4 +82,6 @@ namespace car_try_console
 
     }
 }
+
+
 
--- car_try_console/car_try_console/Program.cs
+++ car_try_console/car_try_console/Program.cs
@@ -15,11 +15,12 @@ var autoWiktora = new Car("Toyota", "Avensis", 4, 3, 2.2);
 var autoBartka = new Car("Bartekmobile", "SuperCustomMade", 2, 3, 10.1);
 var autoPatryka = new Car("Porsche", "Carrera", 5, 5, 15.0);
 
-
+/*
 double spalaniebartka = autoBartka.CalculateFuel(500);
 Console.WriteLine(spalaniebartka);
 double costbartka = autoBartka.CalculateCost(500, 100);
 Console.WriteLine(costbartka);
+*/
 Console.WriteLine($"Our Magnificent factory created new {autoWiktora.Name}, {autoWiktora.Modelname}");
 Console.WriteLine($"Our Magnificent factory created new {autoBartka.Name}, {autoBartka.Modelname} ");
 Console.WriteLine($"Our Magnificent factory created new {autoPatryka.Name}, {autoPatryka.Modelname} ");
