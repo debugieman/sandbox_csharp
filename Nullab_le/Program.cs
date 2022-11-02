46ffe044ac5056dda07e5e5cb6091841cc2bf33d
Debuggieman <107075050+debugieman@users.noreply.github.com>
2022-11-02 08:37:19 +0100

    nullable

---
+++ Nullab_le/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Nullab_le
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? favouritenumber = 11;
+            Console.WriteLine("Favourite number:    " + (favouritenumber.HasValue ? favouritenumber.ToString() : ""));
+        }
+    }
+}
