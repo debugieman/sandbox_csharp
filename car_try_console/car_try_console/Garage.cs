dd61da5e350df0bbb9e9e995bbff17f3bbab8ede
Debuggieman <107075050+debugieman@users.noreply.github.com>
2022-11-03 23:02:25 +0100

    working on class Garage , adding functionality. Park/unpark

--- car_try_console/car_try_console/Garage.cs
+++ car_try_console/car_try_console/Garage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -19,24 +20,77 @@ namespace car_try_console
         private string _adress;
         private int _capacity;
         private int _number_of_cars = 0;
-        private string[] _cars_in = new string[0];
+        private List<Car> _cars_in = new List<Car>();
 
         public string Adress { get { return _adress; } }
         public int Capacity
-        {
+        { get { return _capacity; } 
             set
             {
-                Capacity = value;
-                Cars_in = new cars_in[];
+                _capacity = value;
+                _cars_in = new List<Car>();
 
             }
         }
-        public int Number_of_cars { get { return _number_of_cars; } set { _number_of_cars = value; } }
+        public int Number_of_cars {  get { return _number_of_cars; } set { _number_of_cars = value; } }
         // jak zrobić properkę do tablicy? 
-        public string[] Cars_in { get { return _number_of_cars; } set { _number_of_cars = value; } }
+        public List<Car> Cars_in { get { return _cars_in; } set { _cars_in = value; } }
+
+        public Garage()
+        {
+            _adress = String.Empty;
+            _capacity = 0;
+            _number_of_cars = 0;
+            _cars_in = new List<Car>();
 
 
 
+
+        }
+
+
+        public Garage(string adress, int capacity)
+
+        {
+            Console.WriteLine($"New garage created at {adress}, with {capacity} slots availeable");
+            _cars_in = new List<Car>(capacity);
+            _adress = adress;
+        
+        
+        }
+
+        public void ParkCar(Car car)
+        {
+            if (_cars_in.Count < _cars_in.Capacity)
+            {
+                _cars_in.Add(car);
+                Console.WriteLine($"Car {car} has been parked succesfully");
+
+            }
+            else
+                Console.WriteLine("Parking is full, try again later");
+        }
+
+        public Car? UnPark()
+
+        {
+            Car? to_return = null;
+            if (_cars_in.Count == 0)
+            {
+                Console.WriteLine("Garage is empty");
+
+            }
+            else
+            {   to_return = _cars_in.Last();
+                _cars_in.RemoveAt(Cars_in.Count-1);
+                Console.WriteLine("Car has been unparked :P");
+                
+            
+            }
+            return to_return;
+
+
+        }
 
         //properties PL:  właściwości dostępowe ^^
 
