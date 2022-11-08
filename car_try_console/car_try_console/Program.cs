5789d8b37d7f45c7148aeef6ea1ea386f964820c
Debuggieman <107075050+debugieman@users.noreply.github.com>
2022-11-08 10:35:19 +0100

    ...

--- car_try_console/car_try_console/Program.cs
+++ car_try_console/car_try_console/Program.cs
@@ -13,9 +13,6 @@ auto1.WriteInfo();
 Console.WriteLine($"|Total fulel cost will be : {auto1.CalculateCost(100, 9)}");
 auto2.WriteInfo();
 auto3.WriteInfo();
-//Console.WriteLine($"Our Magnificent factory created new {auto1.Name}, {auto1.Modelname}");
-//Console.WriteLine($"Our Magnificent factory created new {auto2.Name}, {auto2.Modelname} ");
-//Console.WriteLine($"Our Magnificent factory created new {auto3.Name}, {auto3.Modelname} ");
 
 
 Console.WriteLine(Car._numberOfCars);
