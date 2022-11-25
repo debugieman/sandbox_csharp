54817fb1dfc4cac53aef16f56c1c39c5b48a59b9
Debuggieman <107075050+debugieman@users.noreply.github.com>
2022-11-25 02:04:20 +0100

    ;P

--- PhoneBook_list/PhoneBook_list/Program.cs
+++ PhoneBook_list/PhoneBook_list/Program.cs
@@ -5,15 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("PhoneBOOK ************* LIST ***********!");
-        
-        // add contact
-        // dsicplay contact
-        // search by name or number
-        //display all contacts
-        
-        
-        
-        
+
+            // add contact
+            // dsicplay contact
+            // search by name or number
+            //display all contacts
+            Console.WriteLine("1. Add contact");
+
+
+       
         
         
         }
