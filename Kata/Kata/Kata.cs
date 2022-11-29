9d82db224f3af404f49a4717d0f579f8802f6729
Debuggieman <107075050+debugieman@users.noreply.github.com>
2022-11-29 01:13:20 +0100

    practising algorythms and Nunittesting

---
+++ Kata/Kata/Kata.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class Kata
+    {
+        // this is test  -> test is this
+        // tablica ??
+
+        public static string ReverseWords(string str)
+        {
+            return "";
+
+        }
+        public static List<string> Splitter(string input, char separator)
+        { 
+        var result = new List<string>();
+            var  current_index = 0;
+            for (int i; i < input.Length; i++)
+            {
+                if (input[i] == separator)
+                {
+                    var word = input.Substring(current_index, i - current_index);
+                    result.Add(word);
+                    current_index = i + 1;
+
+                }
+                else if (i == input.Lenght - 1)
+                {
+                    var word = input.Substring(current_index, input.Lenght - current_index);
+                    result.Add(word);
+
+                }
+            
+            
+            
+            
+            }
+
+
+            return result; 
+        }
+    
+    
+    
+    
+    }
+}
---
+++ Kata/Kata/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace Kata
+{
+    public class Tests
+    {
+        [SetUp]
+        public void BasicTest()
+
+        { 
+        Assert.AreEqual("world! hello", Kata.ReverseWords("hello world!"));
+      Assert.AreEqual("this like speak doesn't yoda", Kata.ReverseWords("yoda doesn't speak like this"));
+      Assert.AreEqual("foobar", Kata.ReverseWords("foobar"));
+      Assert.AreEqual("kata editor", Kata.ReverseWords("editor kata"));
+      Assert.AreEqual("boat your row row row", Kata.ReverseWords("row row row your boat"));
+            }
+
+
+
+
+
+
+    }
+}
