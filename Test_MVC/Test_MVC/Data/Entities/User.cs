ebfc1133b67c00b8e14c12425cc6bb29f15ba3f9
Debuggieman <107075050+debugieman@users.noreply.github.com>
2023-01-19 20:02:45 +0100

    mvc model with entity preparing

---
+++ Test_MVC/Test_MVC/Data/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test_MVC.Data.Entities
+{
+    public class User : DbContext
+
+    {
+        public DbSet<User> Users { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+    }
+    public classB
+
+}
+
+    
