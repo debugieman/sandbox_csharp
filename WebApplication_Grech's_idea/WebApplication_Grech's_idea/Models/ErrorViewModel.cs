00cba9b82670e57ca8a2fd21d55ec3dc76046fc5
Debuggieman <107075050+debugieman@users.noreply.github.com>
2023-02-10 22:12:08 +0100

    Adding About us view

---
+++ WebApplication_Grech's_idea/WebApplication_Grech's_idea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_Grech_s_idea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
