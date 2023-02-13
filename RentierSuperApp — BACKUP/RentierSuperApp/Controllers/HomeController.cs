66294a9c5547a2195ed5fe5fde4880c5e79c41e4
Debuggieman <107075050+debugieman@users.noreply.github.com>
2023-02-14 00:57:26 +0100

    c.d.

--- RentierSuperApp — BACKUP/RentierSuperApp/Controllers/HomeController.cs
+++ RentierSuperApp — BACKUP/RentierSuperApp/Controllers/HomeController.cs
@@ -28,7 +28,12 @@ namespace RentierSuperApp.Controllers
             return View();
         
         }
-        
+        public IActionResult RealEstates()
+        {
+            return View();
+
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ RentierSuperApp — BACKUP/RentierSuperApp/Models/RealEstateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentierSuperApp.Models
+{
+    public class RealEstateViewModel
+    {
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string City { get; set; }
+        
+
+
+
+
+
+    }
+}
